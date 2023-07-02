ceefa21f80dd00ef01260365cf9f51f4908de5f8
ochudi <chukwudi.ofoma@pau.edu.ng>
2023-07-02 20:05:56 +0100

    Add the flow of exceptions in Java

---
+++ java_concepts/exceptions/Flow/Example.java
@@ -0,0 +1,12 @@
+package java_concepts.exceptions.Flow;
+
+public class Example {
+    // Main driver method
+    public static void main(String args[]) {
+        // Taking an empty string
+        String str = null;
+        // Getting length of a string
+        System.out.println(str.length());
+    }
+
+}
